b7f0f4675bee5626ee348d6343fac42b1ba5e8ba
NicholasNie <glitter66889@foxmail.com>
2017-10-11 23:59:21 +0800

    2017/10/11

--- app/src/main/java/com/nicholasnie/call_with_argora/Activity/CallActivity.java
+++ app/src/main/java/com/nicholasnie/call_with_argora/Activity/CallActivity.java
@@ -1,14 +1,32 @@
 package com.nicholasnie.call_with_argora.Activity;
 
+import android.widget.Button;
+
 import com.nicholasnie.call_with_argora.Base.IView;
 import com.nicholasnie.call_with_argora.Presenter.CallPresenter;
 import com.nicholasnie.call_with_argora.R;
+
+import io.agora.AgoraAPI;
+import io.agora.AgoraAPIOnlySignal;
+import io.agora.rtc.IRtcEngineEventHandler;
+import io.agora.rtc.RtcEngine;
 
 /**
  * Created by NicholasNie on 2017/10/10.
  */
 
 public class CallActivity extends BaseActivity<CallPresenter> implements IView {
+
+    private final String appID = getString(R.string.agora_app_id);
+
+    private final String TAG = "CallActivity";
+
+    private final String roomName = "TestRoom";
+
+    private RtcEngine mRtcEngine;
+    private AgoraAPIOnlySignal mAgoraAPI;
+
+    private Button btnCall;
 
     @Override
     CallPresenter initPresenter() {
@@ -22,11 +40,97 @@ public class CallActivity extends BaseActivity<CallPresenter> implements IView {
 
     @Override
     void initView() {
-
+        btnCall = (Button) findViewById(R.id.btn_test);
     }
 
     @Override
     void onPrepare() {
+        mAgoraAPI = AgoraAPIOnlySignal.getInstance(this,appID);
+        mRtcEngine = RtcEngine.create(this, appID, new IRtcEngineEventHandler() {
+        });
 
+        mAgoraAPI.callbackSet(new AgoraAPI.CallBack(){
+            @Override
+            public void onLoginSuccess(int uid, int fd) {
+                super.onLoginSuccess(uid, fd);
+            }
+
+            @Override
+            public void onLoginFailed(int ecode) {
+                super.onLoginFailed(ecode);
+            }
+
+            @Override
+            public void onLogout(int ecode) {
+                super.onLogout(ecode);
+            }
+
+            @Override
+            public void onLog(String txt) {
+                super.onLog(txt);
+            }
+
+            @Override
+            public void onChannelJoined(String channelID) {
+                super.onChannelJoined(channelID);
+            }
+
+            @Override
+            public void onChannelJoinFailed(String channelID, int ecode) {
+                super.onChannelJoinFailed(channelID, ecode);
+            }
+
+            @Override
+            public void onChannelLeaved(String channelID, int ecode) {
+                super.onChannelLeaved(channelID, ecode);
+            }
+
+            @Override
+            public void onInviteReceived(String channelID, String account, int uid, String extra) {
+                super.onInviteReceived(channelID, account, uid, extra);
+            }
+
+            @Override
+            public void onChannelUserJoined(String account, int uid) {
+                super.onChannelUserJoined(account, uid);
+            }
+
+            @Override
+            public void onChannelUserLeaved(String account, int uid) {
+                super.onChannelUserLeaved(account, uid);
+            }
+
+            @Override
+            public void onChannelUserList(String[] accounts, int[] uids) {
+                super.onChannelUserList(accounts, uids);
+            }
+
+            @Override
+            public void onInviteMsg(String channelID, String account, int uid, String msgType, String msgData, String extra) {
+                super.onInviteMsg(channelID, account, uid, msgType, msgData, extra);
+            }
+
+            @Override
+            public void onInviteReceivedByPeer(String channelID, String account, int uid) {
+                super.onInviteReceivedByPeer(channelID, account, uid);
+            }
+
+            @Override
+            public void onInviteAcceptedByPeer(String channelID, String account, int uid, String extra) {
+                super.onInviteAcceptedByPeer(channelID, account, uid, extra);
+            }
+
+            @Override
+            public void onInviteEndByMyself(String channelID, String account, int uid) {
+                super.onInviteEndByMyself(channelID, account, uid);
+            }
+
+            @Override
+            public void onInviteEndByPeer(String channelID, String account, int uid, String extra) {
+                super.onInviteEndByPeer(channelID, account, uid, extra);
+            }
+        });
+
+        mRtcEngine.enableAudioVolumeIndication(1000,3);
     }
 }
