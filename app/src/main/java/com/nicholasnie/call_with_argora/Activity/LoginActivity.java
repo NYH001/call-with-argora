2eeb27871690467c87111da15303fc230ecc6aec
NicholasNie <glitter66889@foxmail.com>
2017-12-26 22:00:16 +0800

    Modified the UI

--- app/src/main/java/com/nicholasnie/call_with_argora/Activity/LoginActivity.java
+++ app/src/main/java/com/nicholasnie/call_with_argora/Activity/LoginActivity.java
@@ -26,23 +26,23 @@ public class LoginActivity extends BaseActivity<LoginPresenter> implements IView
 
     @Override
     int getLayout() {
-        return R.layout.login_activity;
+        return R.layout.conversation_activity;
     }
 
     @Override
     void initView() {
-        btnLogin = (Button) findViewById(R.id.btn_login1);
-        etUserId = (EditText) findViewById(R.id.et_userId);
-
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                basePresenter.LoginOrSignUp(etUserId.getText().toString().trim());
-                ActivityManager activityManager = ActivityManager.getInstance();
-                activityManager.startActivity(getApplicationContext(),new CallActivity());
-                finish();
-            }
-        });
+//        btnLogin = (Button) findViewById(R.id.btn_login1);
+//        etUserId = (EditText) findViewById(R.id.et_userId);
+//
+//        btnLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                basePresenter.LoginOrSignUp(etUserId.getText().toString().trim());
+//                ActivityManager activityManager = ActivityManager.getInstance();
+//                activityManager.startActivity(getApplicationContext(),new CallActivity());
+//                finish();
+//            }
+//        });
     }
 
     @Override
